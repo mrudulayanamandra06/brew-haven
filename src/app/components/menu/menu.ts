8d2087f703e3e170c6f3ccc39514953b33e0c9b5
Mrudula <yanamandra.lalitha@bcah.christuniversity.in>
2026-07-05 16:05:36 +0530

    Removed menu section and updated contact page

--- src/app/components/menu/menu.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-menu',
-  standalone: true,
-  templateUrl: './menu.html',
-  styleUrl: './menu.css'
-})
-export class MenuComponent {}
